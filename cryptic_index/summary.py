4cc667749c98e8de9445c7cba254ef3eab978519
George Ho <19851673+eigenfoo@users.noreply.github.com>
2021-08-01 19:00:37 -0400

    Add summary script

---
+++ cryptic_index/summary.py
@@ -0,0 +1,77 @@
+import sqlite3
+
+
+def query_and_print(prompt, sql):
+    with sqlite3.connect("cryptics.sqlite3") as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        (output,) = cursor.fetchone()
+    print(f"\t{prompt.ljust(20)}: {str(output).rjust(7)}")
+
+
+queries = {
+    "bigdave44": [
+        ("# crosswords", "select count(1) from raw_bigdave44;"),
+        ("# crosswords parsed", "select count(1) from raw_bigdave44 where is_parsed;"),
+        (
+            "% crosswords parsed",
+            "select printf('%.1f', 100.0 * (select count(1) from raw_bigdave44 where is_parsed) / (select count(1) from raw_bigdave44));",
+        ),
+        ("# clues", "select count(1) from parsed_bigdave44;"),
+        (
+            "# clues reviewed",
+            "select count(1) from parsed_bigdave44 where is_reviewed;",
+        ),
+        (
+            "% clues reviewed",
+            "select printf('%.1f', 100.0 * (select count(1) from parsed_bigdave44 where is_reviewed) / (select count(1) from parsed_bigdave44));",
+        ),
+    ],
+    "fifteensquared": [
+        ("# crosswords", "select count(1) from raw_fifteensquared;"),
+        (
+            "# crosswords parsed",
+            "select count(1) from raw_fifteensquared where is_parsed;",
+        ),
+        (
+            "% crosswords parsed",
+            "select printf('%.1f', 100.0 * (select count(1) from raw_fifteensquared where is_parsed) / (select count(1) from raw_fifteensquared));",
+        ),
+        ("# clues", "select count(1) from parsed_fifteensquared;"),
+        (
+            "# clues reviewed",
+            "select count(1) from parsed_fifteensquared where is_reviewed;",
+        ),
+        (
+            "% clues reviewed",
+            "select printf('%.1f', 100.0 * (select count(1) from parsed_fifteensquared where is_reviewed) / (select count(1) from parsed_fifteensquared));",
+        ),
+    ],
+    "times_xwd_times": [
+        ("# crosswords", "select count(1) from raw_times_xwd_times;"),
+        (
+            "# crosswords parsed",
+            "select count(1) from raw_times_xwd_times where is_parsed;",
+        ),
+        (
+            "% crosswords parsed",
+            "select printf('%.1f', 100.0 * (select count(1) from raw_times_xwd_times where is_parsed) / (select count(1) from raw_times_xwd_times));",
+        ),
+        ("# clues", "select count(1) from parsed_times_xwd_times;"),
+        (
+            "# clues reviewed",
+            "select count(1) from parsed_times_xwd_times where is_reviewed;",
+        ),
+        (
+            "% clues reviewed",
+            "select printf('%.1f', 100.0 * (select count(1) from parsed_times_xwd_times where is_reviewed) / (select count(1) from parsed_times_xwd_times));",
+        ),
+    ],
+}
+
+
+for source, queries_ in queries.items():
+    print(source)
+    for (prompt, query) in queries_:
+        query_and_print(prompt, query)
+    print()
