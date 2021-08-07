1237db5af83d97914a4be8d80b41d33a8c73db03
George Ho <19851673+eigenfoo@users.noreply.github.com>
2021-08-07 13:51:12 -0400

    Cru cryptics

--- cryptic_index/initialize_db.sql
+++ cryptic_index/initialize_db.sql
@@ -70,18 +70,32 @@ CREATE TABLE IF NOT EXISTS parsed_bigdave44 (
   FOREIGN KEY (source_url) REFERENCES raw_bigdave44 (url)
 );
 
-CREATE VIEW IF NOT EXISTS raw AS
-SELECT *
-FROM raw_fifteensquared
-UNION ALL
-    SELECT *
-    FROM raw_times_xwd_times
-UNION ALL
-    SELECT *
-    FROM raw_bigdave44
-;
+-- https://archive.nytimes.com/www.nytimes.com/premium/xword/cryptic-archive.html
+CREATE TABLE IF NOT EXISTS raw_cru_cryptics (
+    url PRIMARY KEY,
+    datetime_requested TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    puz TEXT,
+    is_parsed BOOLEAN DEFAULT FALSE,
+    datetime_parsed TIMESTAMP DEFAULT NULL
+);
+CREATE INDEX IF NOT EXISTS raw_free_puzzes_url_index ON raw_cru_cryptics (url);
+CREATE TABLE IF NOT EXISTS parsed_cru_cryptics (
+  clue TEXT,
+  answer TEXT,
+  definition TEXT,
+  annotation TEXT,
+  clue_number TEXT,
+  puzzle_date TEXT,
+  puzzle_name TEXT,
+  puzzle_url TEXT,
+  source_url TEXT NOT NULL,
+  is_reviewed BOOLEAN DEFAULT FALSE,
+  datetime_reviewed TIMESTAMP DEFAULT NULL,
+  FOREIGN KEY (source_url) REFERENCES raw_bigdave44 (url)
+);
 
-CREATE VIEW IF NOT EXISTS parsed AS
+
+CREATE VIEW IF NOT EXISTS clues AS
 SELECT *
 FROM parsed_fifteensquared
 UNION ALL
@@ -90,6 +104,9 @@ UNION ALL
 UNION ALL
     SELECT *
     FROM parsed_bigdave44
+UNION ALL
+    SELECT *
+    FROM parsed_cru_cryptics
 ;
 
 PRAGMA VACUUM;
