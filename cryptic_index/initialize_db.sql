53de92cb63a918bc8c839d4e1e34a46d13581e3c
George Ho <19851673+eigenfoo@users.noreply.github.com>
2021-08-07 15:03:35 -0400

    Revert back to just HTML

--- cryptic_index/initialize_db.sql
+++ cryptic_index/initialize_db.sql
@@ -70,15 +70,8 @@ CREATE TABLE IF NOT EXISTS parsed_bigdave44 (
   FOREIGN KEY (source_url) REFERENCES raw_bigdave44 (url)
 );
 
+-- Cru Cryptics
 -- https://archive.nytimes.com/www.nytimes.com/premium/xword/cryptic-archive.html
-CREATE TABLE IF NOT EXISTS raw_cru_cryptics (
-    url PRIMARY KEY,
-    datetime_requested TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    puz BLOB,
-    is_parsed BOOLEAN DEFAULT FALSE,
-    datetime_parsed TIMESTAMP DEFAULT NULL
-);
-CREATE INDEX IF NOT EXISTS raw_free_puzzes_url_index ON raw_cru_cryptics (url);
 CREATE TABLE IF NOT EXISTS parsed_cru_cryptics (
   clue TEXT,
   answer TEXT,
@@ -91,7 +84,6 @@ CREATE TABLE IF NOT EXISTS parsed_cru_cryptics (
   source_url TEXT NOT NULL,
   is_reviewed BOOLEAN DEFAULT FALSE,
   datetime_reviewed TIMESTAMP DEFAULT NULL,
-  FOREIGN KEY (source_url) REFERENCES raw_bigdave44 (url)
 );
 
 
--- cryptic_index/populate_db.py
+++ cryptic_index/populate_db.py
@@ -13,13 +13,6 @@ SITEMAP_URLS = {
     "raw_fifteensquared": "https://www.fifteensquared.net/wp-sitemap.xml",
     "raw_times_xwd_times": "https://times-xwd-times.livejournal.com/sitemap.xml",
     "raw_bigdave44": "http://bigdave44.com/sitemap-index-1.xml",
-    "raw_cru_cryptics": "https://archive.nytimes.com/www.nytimes.com/premium/xword/cryptic-archive.html",
-}
-FORMATS = {
-    "raw_fifteensquared": "html",
-    "raw_times_xwd_times": "html",
-    "raw_bigdave44": "html",
-    "raw_cru_cryptics": "puz",
 }
 
 logging.basicConfig(level=logging.INFO)
@@ -91,18 +84,6 @@ def get_new_urls(site):
         urls = {url.text for url in soup.find_all("loc")}
         new_urls = list(urls - known_urls)
 
-    elif "cru_cryptics" in site:
-        sitemap = SITEMAP_URLS[site]
-        response = requests.get(sitemap, headers=headers)
-        soup = bs4.BeautifulSoup(response.text, "lxml")
-        urls = {
-            url["href"]
-            for url in soup.find_all(
-                "a", attrs={"href": lambda s: "puz" in s if s else False}
-            )
-        }
-        new_urls = list(urls - known_urls)
-
     return new_urls
 
 
@@ -123,13 +104,8 @@ if __name__ == "__main__":
 
                 with sqlite3.connect("cryptics.sqlite3") as conn:
                     cursor = conn.cursor()
-                    sql = f"INSERT INTO {site} (url, {FORMATS[site]}) VALUES (?, ?)"
-                    if FORMATS[site] == "html":
-                        cursor.execute(sql, (url, response.text))
-                    elif FORMATS[site] == "puz":
-                        cursor.execute(
-                            sql, (url, sqlite3.Binary(response.text.encode()))
-                        )
+                    sql = f"INSERT INTO {site} (url, html) VALUES (?, ?)"
+                    cursor.execute(sql, (url, response.text))
                     conn.commit()
             except:
                 logging.error(f"Error inserting {url}")
