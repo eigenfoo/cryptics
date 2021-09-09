cb483b447937476eb5a9709e6bd1be13055f62b1
George Ho <19851673+eigenfoo@users.noreply.github.com>
2021-09-08 23:09:09 -0400

    Add FTS across clues and answers

--- scripts/build-db.sh
+++ scripts/build-db.sh
@@ -6,3 +6,4 @@ rm -rf clues.sqlite3
 sqlite3 cryptics/cryptics.sqlite3 ".dump clues" | sqlite3 clues.sqlite3
 sqlite3 clues.sqlite3 ".read cryptics/queries/scrub.sql"
 sqlite3 clues.sqlite3 "vacuum;"
+sqlite-utils enable-fts clues.sqlite3 clues clue answer
