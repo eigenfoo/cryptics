bf544b1ed6e12665df4688299532df0f1d4408b9
George Ho <19851673+eigenfoo@users.noreply.github.com>
2021-10-01 12:28:42 -0400

    Move cryptics.sqlite3 database

--- cryptics/config.py
+++ cryptics/config.py
@@ -5,7 +5,7 @@ import json
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SITEMAPS_JSON = os.path.join(PROJECT_DIR, "sitemaps.json")
-SQLITE_DATABASE = os.path.join(PROJECT_DIR, "cryptics/cryptics.sqlite3")
+SQLITE_DATABASE = os.path.join(PROJECT_DIR, "cryptics.sqlite3")
 
 with open(SITEMAPS_JSON, "r") as f:
     SITEMAPS = json.load(f)
