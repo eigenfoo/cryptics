ff7010af08d4f598720a6c75654ee290cacbe524
George Ho <19851673+eigenfoo@users.noreply.github.com>
2021-11-05 21:37:14 -0400

    Include latest commit id in metadata

--- scripts/build-dbs.sh
+++ scripts/build-dbs.sh
@@ -14,7 +14,8 @@ sqlite3 data.sqlite3 "
 INSERT INTO metadata (key, value)
 VALUES
     ('license', 'This dataset is made available under the Open Database License: http://opendatacommons.org/licenses/odbl/1.0/. Any rights in individual contents of the database are licensed under the Database Contents License: http://opendatacommons.org/licenses/dbcl/1.0/'),
-    ('last_built', '$(date)')
+    ('last_built', '$(date)'),
+    ('last_commit', '$(git log --format="%H" -n 1)')
 "
 sqlite3 data.sqlite3 "VACUUM;"
 sqlite-utils enable-fts data.sqlite3 clues clue answer
@@ -33,6 +34,7 @@ sqlite3 data-annotated.sqlite3 "
 INSERT INTO metadata (key, value)
 VALUES
     ('license', 'This dataset is made available under the Open Database License: http://opendatacommons.org/licenses/odbl/1.0/. Any rights in individual contents of the database are licensed under the Database Contents License: http://opendatacommons.org/licenses/dbcl/1.0/'),
-    ('last_built', '$(date)')
+    ('last_built', '$(date)'),
+    ('last_commit', '$(git log --format="%H" -n 1)')
 "
 sqlite3 data-annotated.sqlite3 "VACUUM;"
