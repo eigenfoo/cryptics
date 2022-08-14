38c120ab3e2fcceacc0dfe56ae871cff3fcf50ec
George Ho <19851673+eigenfoo@users.noreply.github.com>
2022-08-14 17:42:25 -0400

    Refactor SQL queries

--- queries/deduplicate.sql
+++ queries/deduplicate.sql
@@ -1,6 +1,12 @@
+-- Deduplicate
 DELETE FROM clues
-WHERE rowid NOT IN (
-    SELECT MIN(rowid)
+WHERE ROWID NOT IN (
+    SELECT MIN(ROWID)
     FROM clues
     GROUP BY source_url, clue, answer, definition, clue_number, puzzle_date, puzzle_name, puzzle_url
 );
+
+-- Reset ROWIDs
+INSERT INTO clues_renumbered SELECT ROW_NUMBER() OVER (ORDER BY ROWID), * FROM clues;
+DROP TABLE clues;
+ALTER TABLE clues_renumbered RENAME TO clues;
--- queries/reset-clues-rowids.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO clues_renumbered SELECT ROW_NUMBER() OVER (ORDER BY ROWID), * FROM clues;
-DROP TABLE clues;
-ALTER TABLE clues_renumbered RENAME TO clues;
