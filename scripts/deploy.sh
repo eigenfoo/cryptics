3284108a250f36a3799f09791ba101f62ebc7b33
George Ho <19851673+eigenfoo@users.noreply.github.com>
2022-08-20 15:56:55 -0400

    Double SQL time limit to 2s

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -eu -o pipefail
 
 datasette publish heroku data.sqlite3 \
-    --extra-options "--setting suggest_facets off --setting allow_download on --setting truncate_cells_html 0 --setting max_csv_mb 0" \
+    --extra-options "--setting suggest_facets off --setting allow_download on --setting truncate_cells_html 0 --setting max_csv_mb 0 --setting sql_time_limit_ms 2000" \
     --metadata metadata.json \
     --template-dir templates/ \
     --plugins-dir plugins/ \
